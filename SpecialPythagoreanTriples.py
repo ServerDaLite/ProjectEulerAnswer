6b076b5c5c4f4fbc85d484219ac7430b9e54c61b
ServerDaLite <107884123+ServerDaLite@users.noreply.github.com>
2022-10-28 14:46:27 -0400

    Update SpecialPythagoreanTriples.py

--- SpecialPythagoreanTriples.py
+++ SpecialPythagoreanTriples.py
@@ -4,9 +4,12 @@ async def iteration(aMin, bMin, cMin, aMax, bMax, cMax):
     for c in range(cMin, cMax):
         for b in range(bMin, bMax):
             for a in range(aMin, aMax):
-                if (a**2)+(b**2)==(c**2) and a+b+c==1000:
+                if not a+b+c==1000:
+                    break
+                
+                if (a**2)+(b**2)==(c**2):
                     print(a, b, c)
                     break
 
-for i in range(100, 10000, 100):
+for i in range(100, 1000000, 100):
     run(iteration(i-100, i-100, i-100, i, i, i))
